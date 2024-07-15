9aa538322d51a6d87ce45de44295ff57ead823a4
austin.t.tanksley <austin.t.tanksley@gmail.com>
2024-07-15 18:25:37 +0000

    main html in place

---
+++ javascript.js
@@ -0,0 +1,13 @@
+const MY_LIBRARY = [];
+const libList = document.querySelector("#libList");
+
+function book(title, author) {
+    this.title = title;
+    this.author = author;
+}
+
+function displayLib(){
+    for (book in MY_LIBRARY){
+        
+    }
+}
