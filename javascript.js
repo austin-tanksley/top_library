8b904fc0749f85850a62578fccdd9e9bf1582703
austin.t.tanksley <austin.t.tanksley@gmail.com>
2024-07-16 01:52:43 +0000

    finished assignment. Need to improve read status

--- javascript.js
+++ javascript.js
@@ -10,6 +10,8 @@ function Book(title, author) {
 }
 function displayLib(){
     libList.innerHTML = "";
+    let i = 0;
+    const btnRemove = document.createElement("button");
     for (let book of MY_LIBRARY){
         const item = document.createElement("li");
         const title = document.createElement("div");
@@ -19,8 +21,30 @@ function displayLib(){
         author.classList.add("author");
         author.textContent = book.author;
 
-        item.append(title, author);
+        const checkRead = document.createElement("div");
+        const checkbox = document.createElement("input")
+        const labelRead = document.createElement("label")
+        checkbox.type = "checkbox";
+        checkbox.name = "read";
+        labelRead.for = "read";
+        labelRead.textContent = "Read";
+        checkRead.classList.add("checkRead")
+        
+        const delete_button = document.createElement("button");
+        delete_button.textContent = "Remove"
+        delete_button.type = "button";
+        delete_button.dataset.index = `${i}`;
+        delete_button.addEventListener("click", (event) => {
+            const index = event.target.dataset.index;
+            MY_LIBRARY.splice(index,1);
+            displayLib();
+        });
+
+        checkRead.append(labelRead, checkbox)
+        item.append(title, author, checkRead, delete_button);
         libList.append(item);
+
+        i++;
     }
 }
 let current_title = "foo"
@@ -36,10 +60,5 @@ function buttonClick(event){
     current_author = document.getElementById("author").value;
     addBook();
 }
-
-addBook();
-current_title = "FiFi";
-current_author = "your Mom";
-addBook();
 
 form.addEventListener("submit", buttonClick, false);
